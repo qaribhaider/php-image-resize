302c6690bbe5ef53e3e59cd3c40bb5d1636a4d79
Syed Qarib <qaribhaider@gmail.com>
2015-07-28 11:15:29 +0400

    Refactoring

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <form method="post" action="post.php" enctype="multipart/form-data">
-
-	<input type="file" name="myFile" />
-    
+    <input type="file" name="myFile" />
     <input type="submit" value="RESIZE"  />
-    	
 </form>
